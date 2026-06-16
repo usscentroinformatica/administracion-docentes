2f76d4bd0e09499519e00ea1908717ae007b3872
usscentroinformatica <usscentroinformatica@gmail.com>
2026-06-16 09:36:12 -0500

    Update Google Apps Script URL in ListaDocentes

--- src/components/ListaDocentes.jsx
+++ src/components/ListaDocentes.jsx
@@ -38,7 +38,7 @@ const ListaDocentes = ({ onClose, modo = 'admin', docenteId = null }) => {
   const [mostrarCertificaciones, setMostrarCertificaciones] = useState(false);
 
   // URL de tu Google Apps Script (ACTUALIZADA)
-  const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz20XIQEWSIyWZEvkV4ox5lO84K54QUU3b4b3KdG33oeuAy3j1VnSc06_SrIu1lPJxv/exec';
+  const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby7rxwNGSVGUEgYE6X_g4Hk7hUQnZq0abjlHZn1_S80jzyQTlC4HWotn_MwEFu8aykP/exec';
   const GOOGLE_SHEETS_URL = 'https://docs.google.com/spreadsheets/d/1GOJZQDx1XSpudu_80gok1Nuq9YzMvKkLR9fy-jYsyt0/edit';
 
   const abrirGoogleSheets = () => {
