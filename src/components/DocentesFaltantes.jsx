ee47ec7b0007ee7137627a0e3fdfa411fc8625e2
usscentroinformatica <usscentroinformatica@gmail.com>
2026-06-17 17:08:20 -0500

    Update SCRIPT_URL to new Google Apps Script URL

--- src/components/DocentesFaltantes.jsx
+++ src/components/DocentesFaltantes.jsx
@@ -9,7 +9,7 @@ const DocentesFaltantes = () => {
   const [searchTerm, setSearchTerm] = useState('');
 
   // ⚠️ REEMPLAZA con tu URL de Google Apps Script
-  const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxPGHcFF_vyqnO1j6NQQMjq4Fqm51Y4I1BSGeRZ2RWyaKcpTFdPYCo9futOP7f6yteG/exec';
+  const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxMHRznqs3IUJRIBifPx_tSD63r02H7n9-ESSAtkdVcrohiaWBnnW5vTIYPMwhXfn3n/exec';
 
   useEffect(() => {
     cargarDocentesFaltantes();
